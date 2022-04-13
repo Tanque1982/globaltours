e149ae15838b02cfc5572033ff1c860c0e6e011f
Tanque1982 <fernandovercellino@gmail.com>
2022-04-13 16:57:03 -0300

    Seccion 2

--- API/Controllers/LugaresController.cs
+++ API/Controllers/LugaresController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entidades;
+using Core.Interfaces;
 using Infraestructura.Datos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,23 +14,25 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class LugaresController : ControllerBase
     {
-        public ApplicationDbContext Db { get; set; }
-        public LugaresController(ApplicationDbContext db)
+        private readonly ILugarRepositorio _repo;
+
+        public LugaresController(ILugarRepositorio repo)
         {
-            Db = db;
+            _repo = repo;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Lugar>>> GetLugares()
         {
-            var lugares = await Db.Lugar.ToListAsync();
+            var lugares = await _repo.GetLugaresAsync();
 
             return Ok(lugares);
         }
         [HttpGet("ïd")]
         public async Task<ActionResult<Lugar>> GetLugar(int id)
         {
-            return await Db.Lugar.FindAsync(id);
+            return await _repo.GetLugarAsync(id);
         }
     }
 }
