508e18085e7e57219cb7021558b39d0d8306e3d7
Tanque1982 <fernandovercellino@gmail.com>
2022-04-28 16:06:46 -0300

    Seccion 3

--- API/Controllers/LugaresController.cs
+++ API/Controllers/LugaresController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entidades;
+using Core.Especificaciones;
 using Core.Interfaces;
 using Infraestructura.Datos;
 using Microsoft.AspNetCore.Mvc;
@@ -14,25 +17,46 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class LugaresController : ControllerBase
     {
-        private readonly ILugarRepositorio _repo;
+        private readonly IRepositorio<Lugar> _lugarRepo;
+        private readonly IRepositorio<Pais> _paisRepo;
+        private readonly IRepositorio<Categoria> _categoriaRepo;
 
-        public LugaresController(ILugarRepositorio repo)
+        private readonly IMapper _mapper;
+
+        public LugaresController(IRepositorio<Lugar> lugarRepo, IRepositorio<Pais> paisRepo,
+                                 IRepositorio<Categoria> categoriaRepo, IMapper mapper)
         {
-            _repo = repo;
-
+            _mapper = mapper;
+            _categoriaRepo = categoriaRepo;
+            _paisRepo = paisRepo;
+            _lugarRepo = lugarRepo;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Lugar>>> GetLugares()
+        public async Task<ActionResult<IReadOnlyList<LugarDto>>> GetLugares()
         {
-            var lugares = await _repo.GetLugaresAsync();
+            var espec = new LugaresConPaisCategoriaEspecificacion();
+            var lugares = await _lugarRepo.ObtenerTodosEspec(espec);
 
-            return Ok(lugares);
+            return Ok(_mapper.Map<IReadOnlyList<Lugar>,IReadOnlyList<LugarDto>>(lugares));
         }
         [HttpGet("ïd")]
-        public async Task<ActionResult<Lugar>> GetLugar(int id)
+        public async Task<ActionResult<LugarDto>> GetLugar(int id)
         {
-            return await _repo.GetLugarAsync(id);
+            var espec = new LugaresConPaisCategoriaEspecificacion(id);
+            var lugar = await _lugarRepo.ObtenerEspec(espec);
+            return _mapper.Map<Lugar, LugarDto>(lugar);
+        }
+
+        [HttpGet("paises")]
+        public async Task<ActionResult<List<Pais>>> GetPaises()
+        {
+            return Ok(await _paisRepo.ObtenerTodosAsync());
+        }
+        [HttpGet("categoria")]
+        public async Task<ActionResult<List<Categoria>>> GetCategoria()
+        {
+            return Ok(await _categoriaRepo.ObtenerTodosAsync());
         }
     }
 }
